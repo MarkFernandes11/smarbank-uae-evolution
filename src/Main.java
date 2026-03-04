a8a041176895b1672bc99b221b88118cb0ff6547
Oscarf66 <oscarfernandes66@gmail.com>
2026-03-04 22:12:59 +0530

    Added basic funtionalities to check balance, add, withdraw and get transaction history

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
+import com.bank.Account;
+
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner SCANNER = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Welcome To Smartbank!");
+
+        Account account = new Account();
+        boolean exit = false;
+
+        System.out.println("Welcome to your Wallet!");
+        while (!exit) {
+            System.out.println("1. Check your balance");
+            System.out.println("2. Add money");
+            System.out.println("3. Withdraw money");
+            System.out.println("4. Display Transaction History");
+            System.out.println("5. Exit");
+
+            int option = Main.getOption();
+
+            switch (option) {
+                case 1:
+                    account.getBalance();
+                    break;
+                case 2:
+                    System.out.println("Enter amount you want to add");
+                    double amt1 = Double.parseDouble(SCANNER.nextLine());
+                    account.addMoney(amt1);
+                    break;
+                case 3:
+                    System.out.println("Enter amount you want to withdraw");
+                    double amt2 = Double.parseDouble(SCANNER.nextLine());
+                    account.withdrawMoney(amt2);
+                    break;
+                case 4:
+                    account.getTransactionHistory();
+                    break;
+                default :
+                    exit = true;
+                    break;
+            }
+        }
+        System.out.println("Exiting wallet");
+
+        SCANNER.close();
+    }
+
+    private static int getOption() {
+        boolean runLoop = true;
+        int option = 0;
+        while (runLoop) {
+            System.out.println("Choose an option");
+            try {
+                option = Integer.parseInt(SCANNER.nextLine());
+                runLoop = false;
+            } catch (Exception ex) {
+                System.out.println("Invalid input");
+            }
+        }
+        return option;
     }
 }
---
+++ src/com/bank/Account.java
@@ -0,0 +1,46 @@
+package com.bank;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+    private double balance;
+    private List<String> txnHistory = new ArrayList<>();
+
+    public void getBalance() {
+        System.out.println("You have " + balance + " in your account.");
+    }
+
+    public void addMoney(double money) {
+        if (money > 0) {
+            balance += money;
+            System.out.println(money + " added to your account.");
+            txnHistory.add(money + " credited on " + LocalDateTime.now());
+        } else {
+            System.out.println("Invalid input");
+        }
+    }
+
+    public void withdrawMoney(double money) {
+        if (money > 0) {
+            if (balance - money > 0) {
+                balance -= money;
+                System.out.println(money + " withdrawn from your account.");
+                txnHistory.add(money + " withdrawn on " + LocalDateTime.now());
+            } else {
+                System.out.println("You do not have sufficient funds to withdraw.");
+            }
+        } else {
+            System.out.println("Invalid input");
+        }
+    }
+
+    public void getTransactionHistory() {
+        if (txnHistory.isEmpty()) {
+            System.out.println("No transactions to be displayed");
+        } else {
+            txnHistory.forEach(System.out::println);
+        }
+    }
+}
